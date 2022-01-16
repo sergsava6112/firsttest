81f6d36a9b6bfa699349b7d4c05fe26e26816541
Sergey Savelyev <sergsava92@gmail.com>
2022-01-17 01:00:05 +0300

    add new testcase (test_perehod_to_create_zayavka)

--- cian_page.py
+++ cian_page.py
@@ -1,13 +1,17 @@
 from page import BasePage
 from selenium.webdriver.common.by import By
 
-class CianPageLocators:
+class CianMainPageLocators:
     LOCATOR_CIAN_FILTERS_DEMAND = (By.XPATH, '//a[@href = "/podbor-rieltora/"]')
+    LOCATOR_PODBOR = (By.XPATH, '//button[@data-name="SubmitButton"]')
 
 class PerehodHelper(BasePage):
 
     def serch_demand_button(self):
-        return self.find_element(CianPageLocators.LOCATOR_CIAN_FILTERS_DEMAND, time=2).click()
+        return self.find_element(CianMainPageLocators.LOCATOR_CIAN_FILTERS_DEMAND, time=2).click()
+
+    def serch_perehod_bautton(self):
+        return self.find_element(CianMainPageLocators.LOCATOR_PODBOR, time=2).click()
 
 
 
--- test_cian_podbor.py
+++ test_cian_podbor.py
@@ -1,13 +1,22 @@
 from cian_page import PerehodHelper
+import time
 
 
-def test_perehod(driver):
+def test_perehod_to_demand_lending(driver):
     cian_main_page = PerehodHelper(driver)
     cian_main_page.go_to_site()
     assert 'Циан' in driver.title
     cian_main_page.serch_demand_button()
-    assert 'https://spb.cian.ru/podbor-rieltora/' in driver.current_url
+    assert 'https://spb.cian.ru/podbor-rieltora/' or 'https://www.cian.ru/podbor-rieltora/' in driver.current_url
 
+def test_perehod_to_create_zayavka(driver):
+    cian_main_page = PerehodHelper(driver)
+    cian_main_page.go_to_site()
+    assert 'Циан' in driver.title
+    cian_main_page.serch_demand_button()
+    cian_main_page.serch_perehod_bautton()
+    assert 'https://www.cian.ru/application-form/' in driver.current_url
+    time.sleep(5)
 
 
 
