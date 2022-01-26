7596e49f7ea19479d4080ab7046343c7bc4d04c1
Sergey Savelyev <sergsava92@gmail.com>
2022-01-27 02:44:17 +0300

    Correction of remarks

--- cian_page.py
+++ cian_page.py
@@ -2,16 +2,31 @@ from page import BasePage
 from selenium.webdriver.common.by import By
 
 class CianMainPageLocators:
+
     LOCATOR_CIAN_FILTERS_DEMAND = (By.XPATH, '//a[@href = "/podbor-rieltora/"]')
+    LOCATOR_CIAN_FILTERS_KUPIT = (By.XPATH, '//a[@href = "/kupit/"]')
+    LOCATOR_CIAN_FILTERS_SNYAT = (By.XPATH, '//a[@href = "/snyat/"]')
+    LOCATOR_CIAN_FILTERS_POSUTOCHNO = (By.XPATH, '//a[@href = "/posutochno/"]')
+    LOCATOR_CIAN_FILTERS_KALK = (By.XPATH, '//a[@href = "/kalkulator-nedvizhimosti/"]')
+    LOCATOR_CIAN_FILTERS_MORTGAGE = (By.XPATH, '//a[@href = "/mortgage/"]')
+    LOCATOR_BUTTON_POISK = (By.XPATH, '//a[@data-mark="FiltersSearchButton"]')
+    LOCATOR_BATTON_POISK_ON_MAP = (By.XPATH, '//a[@data-mark="FiltersSearchOnMapButton"]')
+    LOCATOR_CIAN_LOGO = (By.XPATH, '//a[@data-name="Logo"]')
+
+class DemandLendingLocators:
     LOCATOR_PODBOR = (By.XPATH, '//button[@data-name="SubmitButton"]')
 
+
 class PerehodHelper(BasePage):
+
+    def open_main_page(self):
+        return self.base_url('https://www.cian.ru')
 
     def serch_demand_button(self):
         return self.find_element(CianMainPageLocators.LOCATOR_CIAN_FILTERS_DEMAND, time=2).click()
 
-    def serch_perehod_bautton(self):
-        return self.find_element(CianMainPageLocators.LOCATOR_PODBOR, time=2).click()
+    def serch_perehod_button(self):
+        return self.find_element(DemandLendingLocators.LOCATOR_PODBOR, time=2).click()
 
 
 
--- test_cian_podbor.py
+++ test_cian_podbor.py
@@ -1,22 +1,24 @@
 from cian_page import PerehodHelper
-import time
+from hamcrest import (
+    assert_that,
+    contains_string,
+)
 
 
-def test_perehod_to_demand_lending(driver):
+def test_open_demand_lending(driver):
     cian_main_page = PerehodHelper(driver)
     cian_main_page.go_to_site()
-    assert 'Циан' in driver.title
     cian_main_page.serch_demand_button()
-    assert 'https://spb.cian.ru/podbor-rieltora/' or 'https://www.cian.ru/podbor-rieltora/' in driver.current_url
+    assert_that(driver.current_url, contains_string('cian.ru/podbor-rieltora/'), 'Не открылся лендинг заявок.')
+
 
 def test_perehod_to_create_zayavka(driver):
     cian_main_page = PerehodHelper(driver)
     cian_main_page.go_to_site()
-    assert 'Циан' in driver.title
     cian_main_page.serch_demand_button()
-    cian_main_page.serch_perehod_bautton()
-    assert 'https://www.cian.ru/application-form/' in driver.current_url
-    time.sleep(5)
+    cian_main_page.serch_perehod_button()
+    assert_that(driver.current_url, contains_string('/application-form/'), 'Не открылась форма создания заявки.')
+
 
 
 
